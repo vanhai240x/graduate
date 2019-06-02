6b40c92c3f8c9bbb7903ef7a6fdef52dfcf53260
Tran Van Hai <tranvanhai240x@gmail.com>
2019-06-02 09:31:34 +0700

    calculator's logic already

--- app.js
+++ app.js
@@ -44,3 +44,17 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
     //             </div>`
     // })
 })
+
+routerApp.controller('HomeCtrl', () => {
+    jQuery(document).ready(function(){
+        jQuery('.skillbar').each(function(){
+            jQuery(this).find('.skillbar-bar').animate({
+                width:jQuery(this).attr('data-percent')
+            },6000);
+        });
+    });
+})
+
+routerApp.controller('CalculatorCtrl', () => {
+    
+})
