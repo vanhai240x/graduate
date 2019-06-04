095580831dad54e75d4324d0b31245c5f4df194b
Noro <ngocro208@gmail.com>
2019-06-04 17:10:39 +0700

    lottery ui

---
+++ README.md
@@ -0,0 +1,5 @@
+font-awesome 5.8.2
+bootstrap 4.3.1
+jquery 3.4.0
+angularjs 1.2.13
+angular-ui-router 0.2.8
--- app.js
+++ app.js
@@ -1,60 +1,79 @@
 var routerApp = angular.module("routerApp", ['ui.router'])
 
 routerApp.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/home')
+  $urlRouterProvider.otherwise('/home')
 
-    $stateProvider
-    // Home states 
+  $stateProvider
+  // Home states 
     .state('home', {
-        url: '/home',
-        templateUrl: 'home/partial-home.html',
-        controller: 'HomeCtrl'
+      url: '/home',
+      templateUrl: 'home/partial-home.html',
+      controller: 'HomeCtrl'
     })
     // Calculator states
     .state('calculator', {
-        url: '/calculator',
-        templateUrl: 'calculator/partial-calculator.html',
-        controller: 'CalculatorCtrl'
+      url: '/calculator',
+      templateUrl: 'calculator/partial-calculator.html',
+      controller: 'CalculatorCtrl'
     })
     // Profile states 
     .state('profile', {
-        url: '/profile',
-        templateUrl: 'profile/partial-profile.html',
-        controller: 'ProfileCtrl'
+      url: '/profile',
+      templateUrl: 'profile/partial-profile.html',
+      controller: 'ProfileCtrl'
     })
     // Lottery states 
     .state('lottery', {
-        url: '/lottery',
-        templateUrl: 'lottery/partial-lottery.html',
-        controller: 'LotteryCtrl'
+      url: '/lottery',
+      templateUrl: 'lottery/partial-lottery.html',
+      controller: 'LotteryCtrl'
     })
 
 
-    // About pages and multiple named views
-    // .state('about', {
-    //     url: '/about',
-    //     template: `<div class="jumbotron">
-    //                 <div class="container">
-    //                 <h1>Hello, world!</h1>
-    //                 <p>Contents ...</p>
-    //                 <p>
-    //                     <a class="btn btn-primary btn-lg">Learn more</a>
-    //                 </p>
-    //                 </div>
-    //             </div>`
-    // })
+  // About pages and multiple named views
+  // .state('about', {
+  //     url: '/about',
+  //     template: `<div class="jumbotron">
+  //                 <div class="container">
+  //                 <h1>Hello, world!</h1>
+  //                 <p>Contents ...</p>
+  //                 <p>
+  //                     <a class="btn btn-primary btn-lg">Learn more</a>
+  //                 </p>
+  //                 </div>
+  //             </div>`
+  // })
 })
 
+// Global controller
+routerApp.controller('GlobalCtrl', () => {
+  // Add class .active to navbar when click nav-item on header
+  $('.nav-1').click(() => {
+    $('.nav-link').removeClass('active')
+    $('.nav-1').addClass('active')
+  })
+  $('.nav-2').click(() => {
+    $('.nav-link').removeClass('active')
+    $('.nav-2').addClass('active')
+  })
+  $('.nav-3').click(() => {
+    $('.nav-link').removeClass('active')
+    $('.nav-3').addClass('active')
+  })
+})
+
+// Home Controller
 routerApp.controller('HomeCtrl', () => {
-    jQuery(document).ready(function(){
-        jQuery('.skillbar').each(function(){
-            jQuery(this).find('.skillbar-bar').animate({
-                width:jQuery(this).attr('data-percent')
-            },6000);
-        });
+  jQuery(document).ready(function() {
+    jQuery('.skillbar').each(function() {
+      jQuery(this).find('.skillbar-bar').animate({
+        width: jQuery(this).attr('data-percent')
+      }, 6000);
     });
+  });
 })
 
+// Calculator controller
 routerApp.controller('CalculatorCtrl', () => {
-    
+
 })
